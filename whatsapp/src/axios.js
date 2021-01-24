82afd357d07407a2daeaaefb8bce1f172c904c25
Aldo <gjikaaldo@gmail.com>
2021-01-24 20:57:00 +0100

    heroku + firebase

--- whatsapp/src/axios.js
+++ whatsapp/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "http://localhost:9000/",
-  baseURL: "https://whatsappaldo.herokuapp.com",
+  baseURL: "http://localhost:9000/",
+  //baseURL: "https://whatsappaldo.herokuapp.com",
 });
 
 export default instance;
